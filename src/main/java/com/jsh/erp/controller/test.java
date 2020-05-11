be37329cc74d8d7c2e1e2e3294f927ea095d6bf9
wujiechen <369091880@qq.com>
2020-05-11 16:42:22 +0800

    first commit

---
+++ src/main/java/com/jsh/erp/controller/test.java
@@ -0,0 +1,13 @@
+package com.jsh.erp.controller;
+
+public class test {
+    public static  void main(String[] args){
+        int total = 0;
+        for(int i =1 ; i<=100;i++){
+            total += i;
+        }
+        System.out.println("HELLO WORLD");
+    }
+
+
+}
